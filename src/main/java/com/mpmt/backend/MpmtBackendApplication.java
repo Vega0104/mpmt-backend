9d4c561879ead64ef34320ee4e04ef04891d3654
Gael <108774720+Gaeljs@users.noreply.github.com>
2025-07-01 17:43:19 +0200

    chore:init du projet Spring Boot

---
+++ src/main/java/com/mpmt/backend/MpmtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mpmt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpmtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpmtBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpmt/backend/MpmtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpmt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpmtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
